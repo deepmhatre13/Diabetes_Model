21bb012db075e51218d8ee256f32bc09206da994
Deep Mhatre <mhatredeepam@gmail.com>
2025-07-11 23:09:00 +0530

    Updated Form.jsx, added requirements.txt, and updated .gitignore to ignore .env

--- project/src/components/Form.jsx
+++ project/src/components/Form.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import { motion } from 'framer-motion';
 import ResultCard from './ResultCard';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-const API_BASE_URL = 'http://localhost:8000/api';
 
 const Form = () => {
   const [formData, setFormData] = useState({
